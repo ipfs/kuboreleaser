8ee26058d3370cbb70d3ea2e26085f5faa33374e
galargh <piotr.galar@gmail.com>
2023-07-03 15:40:34 +0200

    feat: allow skipping matrix setup

--- cmd/kuboreleaser/main.go
+++ cmd/kuboreleaser/main.go
@@ -162,7 +162,7 @@ func main() {
 							&cli.BoolFlag{
 								Name:  "skip-matrix",
 								Usage: "Do not use Matrix client",
-								Value: false,
+								Value: os.Getenv("NO_MATRIX") != "",
 							},
 						},
 						Action: func(c *cli.Context) error {
