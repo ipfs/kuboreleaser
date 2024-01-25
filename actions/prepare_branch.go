8908292dcd0da8dcfe673a578f945e3b1d35eb94
Henrique Dias <mail@hacdias.com>
2024-01-25 10:11:41 +0100

    actions: do not ask to merge release PR for prereleases

--- actions/prepare_branch.go
+++ actions/prepare_branch.go
@@ -319,8 +319,13 @@ Please approve after all the required commits are cherry-picked.`, branch, repos
 		if err != nil {
 			return err
 		}
-		if !pr.GetMerged() && !util.ConfirmPR(pr) {
-			return fmt.Errorf("%s not merged", pr.GetHTMLURL())
+
+		if ctx.Version.IsPrerelease() {
+			fmt.Printf(`Release PR ready at %s. Do not merge it.`, pr.GetHTMLURL())
+		} else {
+			if !ctx.Version.IsPrerelease() && !pr.GetMerged() && !util.ConfirmPR(pr) {
+				return fmt.Errorf("%s not merged", pr.GetHTMLURL())
+			}
 		}
 	}
 
